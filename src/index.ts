ec5cfd3366b743eff80455bd4d1179faaeb6daae
Evan Doocy <e771076@safetydirect.info>
2024-09-16 16:54:42 -0700

    Fixed formatting

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,32 @@ import * as protobuf from 'protobufjs';
 import * as path from 'path';
 
 async function main() {
-const root = await protobuf.load(path.join(__dirname, 'proto', 'person.proto'));
-const Person = root.lookupType('Person');
+  const root = await protobuf.load(path.join(__dirname, 'proto', 'person.proto'));
+  const Person = root.lookupType('Person');
 
-const payload = {
-  name: 'John Doe',
-  id: 1234,
-  email: 'john.doe@example.com'
-};
+  const payload = {
+    name: 'John Doe',
+    id: 1234,
+    email: 'john.doe@example.com'
+  };
 
-const errMsg = Person.verify(payload);
-if (errMsg) {
-  throw Error(errMsg);
-}
+  const errMsg = Person.verify(payload);
+  if (errMsg) {
+    throw Error(errMsg);
+  }
 
-const message = Person.create(payload);
-const buffer = Person.encode(message).finish();
+  const message = Person.create(payload);
+  const buffer = Person.encode(message).finish();
 
-console.log("Encoded buffer: ", buffer);
+  console.log("Encoded buffer: ", buffer);
 
-const decodedMessage = Person.decode(buffer);
-console.log("Decoded message: ", decodedMessage);
+  const decodedMessage = Person.decode(buffer);
+  console.log("Decoded message: ", decodedMessage);
 
-const object = Person.toObject(decodedMessage);
+  const object = Person.toObject(decodedMessage);
 
-console.log("Decoded object: ", object);
- 
+  console.log("Decoded object: ", object);
+
 }
 
 main().catch(err => console.error(err));
